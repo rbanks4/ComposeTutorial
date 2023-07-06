9f7829c572823497758d3a8bc5992f678af2a77e
Reggie Jr <p3anutbj7@gmail.com>
2023-07-06 10:20:35 -0400

    finished tutorial

--- app/src/main/java/com/gaming/android/composetutorial/MainActivity.kt
+++ app/src/main/java/com/gaming/android/composetutorial/MainActivity.kt
@@ -1,15 +1,28 @@
 package com.gaming.android.composetutorial
 
+import SampleData
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.gaming.android.composetutorial.ui.theme.ComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +30,85 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTutorialTheme {
-                // A surface container using the 'background' color from the theme
+                Conversation(SampleData.conversationSample)
+            }
+        }
+    }
+
+    @Composable
+    fun MessageCard(msg: Message) {
+        Row(modifier = Modifier.padding(all = 8.dp)) {
+            Image(
+                painter = painterResource(id = R.drawable.feathered_edge_intact),
+                contentDescription = "feathered edge from Tears of the Kingdom",
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+                    .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            var isExpanded by remember { mutableStateOf(false) }
+            val surfaceColor by animateColorAsState(
+                if(isExpanded) MaterialTheme.colorScheme.primary
+                else MaterialTheme.colorScheme.surface
+            )
+
+            Column (modifier = Modifier.clickable { isExpanded = !isExpanded }){
+                Text(
+                    text = msg.author,
+                    color = MaterialTheme.colorScheme.secondary,
+                    style = MaterialTheme.typography.titleSmall
+                )
+
+                Spacer(modifier = Modifier.height(4.dp))
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    shape = MaterialTheme.shapes.medium,
+                    shadowElevation = 1.dp,
+                color = surfaceColor, 
+                    modifier = Modifier.animateContentSize().padding(1.dp)
                 ) {
-                    Greeting("Android")
+                    Text(
+                        text = msg.body,
+                        modifier = Modifier.padding(all = 4.dp),
+                        maxLines = if(isExpanded) Int.MAX_VALUE else 1,
+                        style = MaterialTheme.typography.bodyMedium
+                    )
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+    @Composable
+    fun Conversation(messages: List<Message>){
+        LazyColumn {
+            items(messages) { message ->
+                MessageCard(message)
+            }
+        }
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeTutorialTheme {
-        Greeting("Android")
+    @Preview(name = "Light Mode")
+    @Preview(
+        uiMode = Configuration.UI_MODE_NIGHT_YES,
+        showBackground = true,
+        name = "Dark Mode"
+    )
+    @Composable
+    fun PreviewMessageCard() {
+        ComposeTutorialTheme {
+            Surface {
+                MessageCard(Message("Ann", "How are you?"))
+            }
+        }
+    }
+
+    @Preview
+    @Composable
+    fun PreviewConversation() {
+        ComposeTutorialTheme {
+            Conversation(SampleData.conversationSample)
+        }
     }
 }
---
+++ app/src/main/java/com/gaming/android/composetutorial/Message.kt
@@ -0,0 +1,3 @@
+package com.gaming.android.composetutorial
+
+data class Message(val author: String, val body: String)
